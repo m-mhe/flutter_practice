395d1cef1adfa76789ee01c81cb418c6bde87425
MMHE <mominhasanemon@gmail.com>
2024-04-02 19:48:22 +0600

    First commit

--- project_one/lib/main.dart
+++ project_one/lib/main.dart
@@ -1,24 +1,201 @@
 //To format code - ctrl + alt + L
 
-//Import section:
 import 'package:flutter/material.dart';
-import 'package:project_one/home.dart';
+import 'package:project_one/settings.dart';
 
-//Code execution point:
-void main() {
+void main(){
   runApp(const MyApp());
 }
 
-//This custom widget contain MaterialApp design system:
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: "Navigation Tester",
-      //This page will be shown as home:
-      home: HomeRoute(),
+    return MaterialApp(
+      title: "ThemeData()",
+      themeMode: ThemeMode.light,
+      theme:ThemeData(
+        brightness: Brightness.light,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          )
+            //We can't set theme for InkWell(), and GestureDetector()
+        ),
+        iconButtonTheme: IconButtonThemeData(
+          style: IconButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue
+          )
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.blue,
+          )
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7)
+            )
+          )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blue),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(color: Colors.blue)
+          )
+        ),
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.blue,
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.black,
+            foregroundColor: Colors.blue,
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+            )
+          //We can't set theme for InkWell(), and GestureDetector()
+        ),
+        iconButtonTheme: IconButtonThemeData(
+            style: IconButton.styleFrom(
+                foregroundColor: Colors.blue,
+                backgroundColor: Colors.black
+            )
+        ),
+        textButtonTheme: TextButtonThemeData(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.blue,
+              backgroundColor: Colors.black,
+            )
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.black,
+                foregroundColor: Colors.blue
+            )
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+                borderRadius: BorderRadius.circular(10)
+            ),
+            focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20),
+                borderSide: BorderSide(color: Colors.white)
+            )
+        ),
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget{
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.home),
+        title: Text("HOME",
+        style: TextStyle(
+          color: Colors.blue
+        ),),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.push(context,
+                MaterialPageRoute(builder: (context){
+                  return Setting();
+                })
+              );
+            },
+            icon: Icon(Icons.settings),
+          )
+        ],
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: (){
+                Navigator.push(context,
+                  MaterialPageRoute(builder:(context){
+                    return const Setting();
+                  })
+                );
+              },
+              icon: Icon(Icons.settings)
+            ),
+            TextButton(
+                onPressed: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder:(context){
+                        return const Setting();
+                      })
+                  );
+                },
+                child: Text("Setting")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder:(context){
+                        return const Setting();
+                      })
+                  );
+                },
+                child: Text("Setting")
+            ),
+            InkWell(
+              onDoubleTap: (){
+                Navigator.push(context,
+                    MaterialPageRoute(builder:(context){
+                      return const Setting();
+                    })
+                );
+              },
+              child: Text("Double Tap")
+            ),
+            GestureDetector(
+                onHorizontalDragEnd: (c){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder:(context){
+                        return const Setting();
+                      })
+                  );
+                },
+                child: Column(
+                  children: [
+                    Text("Slide"),
+                    Text("<-                                                              ->"),
+                    Text("Left or Right")
+                  ],
+                )
+            ),
+            TextField()
+          ],
+        )
+      )
     );
   }
 }
---
+++ project_one/lib/settings.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:project_one/main.dart';
+
+class Setting extends StatelessWidget{
+  const Setting({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SETTING"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.home)
+            ),
+            TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text("HOME")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.pop(context);
+                },
+                child: Text("Home")
+            ),
+            InkWell(
+                onDoubleTap: (){
+                  Navigator.pop(context);
+                },
+                child: Text("Double Tap")
+            ),
+            GestureDetector(
+              onHorizontalDragEnd: (c){
+                if((c.primaryVelocity ?? 0) < 0){
+                  Navigator.pushAndRemoveUntil(context,
+                    MaterialPageRoute(builder: (context){
+                      return const Home();
+                    }),
+                      (route) => false
+                  );
+                }
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Slide"),
+                  Text("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"),
+                  Text("To Left")
+                ],
+              ),
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
